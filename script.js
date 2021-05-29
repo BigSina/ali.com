63795509706485e7dd4537f4a50d0e6a34daec5a
BigSina <74648226+BigSina@users.noreply.github.com>
2021-05-29 17:25:30 +0430

    changed

--- script.js
+++ script.js
@@ -13,25 +13,22 @@ const nextBtn = document.getElementById('next');
 // Music
 const songs = [
   {
-    name: 'jacinto-1',
-    displayName: 'Electric Chill Machine',
-    artist: 'Jacinto Design',
+    name: 'a',
+    displayName: 'Adelane Nist',
+    artist: 'Reza Bahram',
   },
   {
-    name: 'jacinto-2',
-    displayName: 'Seven Nation Army (Remix)',
-    artist: 'Jacinto Design',
+    name: 'c',
+    displayName: 'Behet Ghol Midam',
+    artist: 'Mohsen Yeganeh',
   },
+  
   {
-    name: 'jacinto-3',
-    displayName: 'Goodnight, Disco Queen',
-    artist: 'Jacinto Design',
+    name: 'b',
+    displayName: 'Waqqaberry',
+    artist: 'BÖ',
   },
-  {
-    name: 'metric-1',
-    displayName: 'Front Row (Remix)',
-    artist: 'Metric/Jacinto Design',
-  },
+  
 ];
 
 // Check if Playing
